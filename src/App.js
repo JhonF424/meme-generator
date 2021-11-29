f3c7ce2fd9fe302e991f3522d7d27f92ad651040
Jhon Alejandro Franco Gómez <jhon@remotepcsolutions.com>
2021-11-29 16:41:48 -0500

    Inicialización y configuración de React y Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+
+  //Hooks
+  const [firstLine, setFirstLine] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container flex flex-col mx-auto mt-4 w-4/5 md:w-1/2">
+        <div className="">
+          <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline my-4">
+            <option value="">Niña incendio</option>
+            <option value="">Futurama</option>
+            <option value="">Aliens</option>
+            <option value="">Matrix</option>
+            <option value="">Filosoraptor</option>
+            <option value="">Smart Nigga</option>
+          </select>
+        </div>
+        <input className="input-form" type="text" placeholder="First Text"></input>
+        <input className="input-form" type="text" placeholder="Second Text"></input>
+        <button className="button-form">Generate meme</button>
+
+        <div>
+          <span>First Line</span>
+          <span>Second Line</span>
+          <img src="" />
+        </div>
+      </div>
     </div>
   );
 }
